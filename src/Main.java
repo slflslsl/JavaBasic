626a544e2992639ff9bd53d371b9a2990d0f4864
slflsll <qpalzm4329@gmail.com>
2026-08-06 02:30:55 +0900

    heal 메서드와 limitHp 메서드 추가

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
 public class Main {
-
     static int heal(int amount) {
         return amount;
     }
+    static int limitHp(int hp) {
 
+        if (hp > 100) {
+            hp = 100;
+        }
+
+        return hp;
+    }
     public static void main(String[] args) {
-
         int playerHp = 50;
-
-        playerHp += heal(10);
-        System.out.println("HP : " + playerHp);
-        playerHp += heal(20);
-        System.out.println("HP : " + playerHp);
         playerHp += heal(30);
+        playerHp = limitHp(playerHp);
         System.out.println("HP : " + playerHp);
-
+        playerHp += heal(80);
+        playerHp = limitHp(playerHp);
+        System.out.println("HP : " + playerHp);
+        playerHp += heal(100);
+        playerHp = limitHp(playerHp);
+        System.out.println("HP : " + playerHp);
     }
 }
