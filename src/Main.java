e7f8da3bc5e90144e8e6757729a3db7eeea64134
slflsll <qpalzm4329@gmail.com>
2026-08-11 04:24:46 +0900

    미니프로젝트

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,20 @@
 public class Main {
-
     static int attack(int playerHp, int damage) {
         System.out.println("공격!");
-
         if (playerHp - damage <= 0) {
             return 0;
         }
-
         return playerHp - damage;
     }
-
     public static void main(String[] args) {
-
-        int[] playerHp = {100, 80, 50};
+        int[] playerHp = {100, 70, 40};
         int damage = 30;
-
         for (int i = 0; i < playerHp.length; i++) {
-            playerHp[i] = attack(playerHp[i], damage);
-            System.out.println("플레이어 HP : " + playerHp[i]);
+            while (playerHp[i] > 0) {
+                playerHp[i] = attack(playerHp[i], damage);
+                System.out.println("플레이어 " + (i + 1) + " HP : " + playerHp[i]);
+            }
+            System.out.println("플레이어 " + (i + 1) + " : 사망");
         }
     }
 }
