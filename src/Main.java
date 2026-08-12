1d70bc67445582a870c4c7d09a8e221b273ab73f
slflsll <qpalzm4329@gmail.com>
2026-08-12 12:30:37 +0900

    클래스 키보드 마우스

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
+
+class Product {
+    String name;
+    int price;
+    int stock;
+
+    Product(String name, int price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+}
 public class Main {
-
-    static int heal(int playerHp, int amount) {
-        if (playerHp + amount >= 100) {
-            System.out.println("체력이 가득 찼습니다!");
-            return 100;
-        }
-
-        System.out.println("회복했습니다!");
-        return playerHp + amount;
-    }
-
-    public static void main(String[] args) {
-
-        int playerHp = 70;
-
-        playerHp = heal(playerHp, 20);
-        System.out.println("HP : " + playerHp);
-
-        playerHp = heal(playerHp, 20);
-        System.out.println("HP : " + playerHp);
-    }
+public static void main(String[] args) {
+    Product keyboard = new Product("키보드", 50000, 10);
+    Product mouse = new Product("마우스", 30000, 20);
+    System.out.println("상품명: " + keyboard.name);
+    System.out.println("가격: " + keyboard.price);
+    System.out.println("재고: " + keyboard.stock);
+    System.out.println();
+    System.out.println("상품명: " + mouse.name);
+    System.out.println("가격: " + mouse.price);
+    System.out.println("재고: " + mouse.stock);
+}
 }
