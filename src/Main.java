a668298c36b07683c536d33dec16c3e34276ee57
slflsll <qpalzm4329@gmail.com>
2026-08-13 05:20:47 +0900

    객체지향 order

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 class Product {
     String name;
     int price;
@@ -15,27 +14,45 @@ class Product {
             System.out.println("재고 부족");
             return false;
         }
+
         stock -= quantity;
         return true;
     }
 }
+
+class Order {
+    String customerName;
+    Product product;
+    int quantity;
+
+    Order(String customerName, Product product, int quantity) {
+        this.customerName = customerName;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    boolean placeOrder() {
+        return product.decreaseStock(quantity);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
 
         Product keyboard = new Product("키보드", 50000, 5);
 
-        int quantity = 6;
+        Order order = new Order("철수", keyboard, 2);
 
-        boolean success = keyboard.decreaseStock(quantity);
+        boolean success = order.placeOrder();
 
         if (success) {
             System.out.println("주문 성공");
-            System.out.println("주문 수량: " + quantity);
-            System.out.println("주문 후 재고: " + keyboard.stock);
+            System.out.println("고객명 : " + order.customerName);
+            System.out.println("상품명 : " + order.product.name);
+            System.out.println("주문 수량 : " + order.quantity);
+            System.out.println("주문 후 재고 : " + order.product.stock);
         } else {
             System.out.println("주문 실패");
-            System.out.println("주문 수량: " + quantity);
-            System.out.println("현재 재고: " + keyboard.stock);
         }
     }
 }
