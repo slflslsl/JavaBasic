208ad9eb9a4d30af6ad02db694e21d81a9c7eba4
slflsll <qpalzm4329@gmail.com>
2026-08-20 18:02:48 +0900

    private

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,20 @@
 class Product {
-    String name;
-    int price;
-    int stock;
+    private String name;
 
-    Product(String name, int price, int stock) {
+    Product(String name) {
         this.name = name;
-        this.price = price;
-        this.stock = stock;
     }
 
-    boolean decreaseStock(int quantity) {
-        if (stock < quantity) {
-            System.out.println("재고 부족");
-            return false;
-        }
-
-        stock -= quantity;
-        return true;
-    }
-}
-
-class Order {
-    String customerName;
-    Product product;
-    int quantity;
-
-    Order(String customerName, Product product, int quantity) {
-        this.customerName = customerName;
-        this.product = product;
-        this.quantity = quantity;
-    }
-
-    boolean placeOrder() {
-        return product.decreaseStock(quantity);
+    public String getName() {
+        return name;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
 
-        Product keyboard = new Product("키보드", 50000, 5);
+        Product product = new Product("키보드");
 
-        Order order = new Order("철수", keyboard, 2);
-
-        boolean success = order.placeOrder();
-
-        if (success) {
-            System.out.println("주문 성공");
-            System.out.println("고객명 : " + order.customerName);
-            System.out.println("상품명 : " + order.product.name);
-            System.out.println("주문 수량 : " + order.quantity);
-            System.out.println("주문 후 재고 : " + order.product.stock);
-        } else {
-            System.out.println("주문 실패");
-        }
+        System.out.println(product.getName());
     }
 }
